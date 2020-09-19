7a08b23f90daa580b31c8f52bc7d55bae5bd876a
Kushtar <eseptehelp@gmail.com>
2020-09-19 06:41:42 +0600

    Добавьте файлы проекта.

---
+++ EsepteSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EsepteSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
